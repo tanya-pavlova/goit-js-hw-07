3621919a66d733cad07fa7908e2f01f87c4f42cd
tanya-pavlova <70815370+tanya-pavlova@users.noreply.github.com>
2021-02-02 00:24:49 +0300

    исправления task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,11 @@
 const inputRef = document.querySelector('input#name-input');
 const outputRef = document.querySelector('span#name-output');
 
-inputRef.addEventListener('input', event => {
-outputRef.textContent = event.target.value;
-});
+inputRef.addEventListener ('input', onInputChange);
+
+function onInputChange(event) {
+    outputRef.textContent = event.target.value
+    ?event.target.value
+    :'незнакомец';
+
+}
